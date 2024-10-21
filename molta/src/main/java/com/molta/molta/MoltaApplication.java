cd2aeecd4b7accd7ffb9258ac6607d37c5d3c9b5
ddeodeok <kimdepkhyeon@gmail.com>
2024-10-21 19:42:29 +0900

    Initial commit

---
+++ molta/src/main/java/com/molta/molta/MoltaApplication.java
@@ -0,0 +1,13 @@
+package com.molta.molta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoltaApplication.class, args);
+	}
+
+}
---
+++ molta/src/test/java/com/molta/molta/MoltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molta.molta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
